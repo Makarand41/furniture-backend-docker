a20fed2b9da170068d7b8c1e8c0f68978dadeb51
Makarand41 <makarandpatil67@gmail.com>
2025-10-29 19:53:22 +0530

    added corsconfig file

---
+++ src/main/java/com/nanotech/furniture/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.nanotech.furniture.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "http://localhost:3000",
+                                "https://furniture-frontend-teck.vercel.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .exposedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
